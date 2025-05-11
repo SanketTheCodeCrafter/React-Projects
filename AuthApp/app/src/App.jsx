870441acd978fe58267ce3ffe91dce91b0e5234e
SanketTheCodeCrafter <sanket.nagap3@gmail.com>
2025-05-11 12:52:23 +0530

    Basic setup of project. Added routes and create pages

---
+++ AuthApp/app/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
